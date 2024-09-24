0977ec8da71cada6c86615b523274136cbab30a9
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-09-24 12:12:05 -0300

    feat: Service para gerenciamento de estado de travels e injecao de dependencia na raiz do app.

---
+++ lib/services/travels_service.dart
@@ -0,0 +1,109 @@
+import 'package:bike/models/part_model.dart';
+import 'package:bike/models/travel_model.dart';
+import 'package:bike/services/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TravelsException implements Exception {
+  String message;
+  TravelsException(this.message);
+}
+
+class TravelService extends ChangeNotifier {
+  List<Travel>? travels;
+  AuthService? _authService;
+
+  void setAuthService(AuthService authService) {
+    _authService = authService;
+  }
+ 
+
+  _getTravels() async {
+    travels = await getTravels();
+  }
+
+  _setTravels(List<Travel> travel) {
+    travels = travel;
+    notifyListeners();
+  }
+
+  Future<void> addTravel(Travel newTravel) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    List<Travel> travel = [];
+
+    if (_authService!.dbUser != null && _authService!.dbUser!.id != null) {
+      int? id = _authService!.dbUser!.id;
+
+      QuerySnapshot userSnapshot =
+          await db.collection('user').where('id', isEqualTo: id).get();
+
+      if (userSnapshot.docs.isNotEmpty) {
+        print("Documento encontrado");
+        DocumentSnapshot userDoc = userSnapshot.docs.first;
+
+        CollectionReference travelCollection =
+            db.collection("user").doc(userDoc.id).collection("travels");
+
+        print("Colecao encontrada");
+
+        await travelCollection.add(newTravel.toJson());
+        print("Adicioanado com sucesso");
+
+        QuerySnapshot travelSnapshot = await db
+            .collection("user")
+            .doc(userDoc.id)
+            .collection("travels")
+            .get();
+
+        travel = travelSnapshot.docs.map((elem) {
+          return Travel.fromJson(elem.data() as Map<String, dynamic>);
+        }).toList();
+
+        _setTravels(travel);
+      }
+
+      notifyListeners();
+    } else {
+      print("Erro: Usuário não encontrado ou ID inválido.");
+    }
+  }
+
+  Future<List<Travel>> getTravels() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    List<Travel> travel = [];
+
+    if (_authService!.dbUser != null && _authService!.dbUser!.id != null) {
+      int? userId = _authService!.dbUser!.id;
+
+      QuerySnapshot userSnapshot =
+          await db.collection('user').where('id', isEqualTo: userId).get();
+
+      if (userSnapshot.docs.isNotEmpty) {
+        DocumentSnapshot userDoc = userSnapshot.docs.first;
+
+        QuerySnapshot travelSnapshot = await db
+            .collection("user")
+            .doc(userDoc.id)
+            .collection("travels")
+            .get();
+
+        travel = travelSnapshot.docs.map((elem) {
+          print("travel.data()");
+          print(elem.data());
+          return Travel.fromJson(elem.data() as Map<String, dynamic>);
+        }).toList();
+
+        print("Travels length");
+        print(travel.length);
+        _setTravels(travel);
+      } else {
+        print("Usuário não encontrado.");
+      }
+    } else {
+      print("Erro: Usuário não encontrado ou ID inválido.");
+    }
+
+    return travel;
+  }
+}
