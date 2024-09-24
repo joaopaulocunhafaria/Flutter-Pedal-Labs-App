e6f082e9826dfd7525fe8a089bcec8c1f619e50a
Joao Paulo Cunha <joaopaulofaria98@gmail.com>
2024-09-24 14:52:41 -0300

    feat: Redirecionamento para tela de nova viagem.

--- lib/pages/travels/travels_page.dart
+++ lib/pages/travels/travels_page.dart
@@ -1,5 +1,7 @@
 import 'package:bike/models/bike_model.dart';
 import 'package:bike/models/travel_model.dart';
+import 'package:bike/pages/travels/new_travel_page.dart';
+import 'package:bike/pages/travels/travel_info_page.dart';
 import 'package:bike/services/bike_service.dart';
 import 'package:bike/services/travels_service.dart';
 import 'package:bike/widgets/app_bar.dart';
@@ -33,15 +35,14 @@ class _TravelsState extends State<Travels> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              
               Expanded(
                   flex: 1,
                   child: Padding(
-                    padding: const EdgeInsets.only(top:18.0),
+                    padding: const EdgeInsets.only(top: 18.0),
                     child: Center(
                       child: Text("Histórico de Viagens",
                           style: GoogleFonts.acme(
-                            color: Colors.blue,
+                            color: const Color.fromARGB(255, 0, 0, 0),
                             fontSize: 35,
                             fontWeight: FontWeight.bold,
                             letterSpacing: 2,
@@ -82,10 +83,20 @@ class _TravelsState extends State<Travels> {
                       }
                       return ListView.builder(
                         itemCount: travels.length,
-                        itemBuilder: (context, index)   { 
+                        itemBuilder: (context, index) {
                           return InkWell(
-                            onTap: () {
-                              //navegar para tela de informacoes de viagens
+                            onTap: () async {
+                              Bike? bike = await bikeService
+                                  .getBikeById(travels[index].bikeId!);
+
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => TravelInfo(
+                                    travel: travels[index],
+                                    bike: bike!,
+                                  ),
+                                ),
+                              );
                             },
                             child: Card(
                               elevation: 8,
@@ -121,12 +132,12 @@ class _TravelsState extends State<Travels> {
                                               fontWeight: FontWeight.bold,
                                               letterSpacing: 2,
                                             ),
-                                          ), 
+                                          ),
                                           Text(
-                                            
-                                                travels[index]
+                                            travels[index]
                                                     .distance!
-                                                    .toString()+" Km Percorridos",
+                                                    .toString() +
+                                                " Km Percorridos",
                                             style: GoogleFonts.acme(
                                               color: Colors.blue,
                                               fontSize: 14,
@@ -159,6 +170,201 @@ class _TravelsState extends State<Travels> {
                   },
                 ),
               ),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9),
+                        color: Colors.blue),
+                    margin: EdgeInsets.all(10),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Center(
+                              child: Text(
+                                "Escolha uma bike!",
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0)),
+                              ),
+                            ),
+                            actions: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                    FutureBuilder<List<Bike>>(
+                                      future: bikeService.getBikes(),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.hasError) {
+                                          return Center(
+                                            child: Text(
+                                              "Erro ao carregar bikes",
+                                              style: GoogleFonts.acme(
+                                                color: Colors.blue,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                                letterSpacing: 2,
+                                              ),
+                                            ),
+                                          );
+                                        } else if (snapshot.hasData) {
+                                          List<Bike> bikes = snapshot.data!;
+                                          if (bikes.isEmpty) {
+                                            return Center(
+                                              child: Text(
+                                                "Você não tem nenhuma bike registrada.",
+                                                style: GoogleFonts.acme(
+                                                  color: Colors.blue,
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                  letterSpacing: 2,
+                                                ),
+                                              ),
+                                            );
+                                          }
+                                          // ListView baseado no número de bikes
+                                          return Container(
+                                            width: 250,
+                                            height: 400,
+                                            child: ListView.builder(
+                                              itemCount: bikes.length,
+                                              itemBuilder: (context, index) {
+                                                return InkWell(
+                                                  onTap: () {
+                                                    Navigator.of(context)
+                                                        .pushReplacement(
+                                                      MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            NewTravelPage(
+                                                          bike: bikes[index],
+                                                        ),
+                                                      ),
+                                                    );
+                                                  },
+                                                  child: Card(
+                                                    elevation: 8,
+                                                    margin:
+                                                        const EdgeInsets.all(12),
+                                                    child: Padding(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              16),
+                                                      child: Row(
+                                                        children: [
+                                                          const Expanded(
+                                                              flex: 2,
+                                                              child: Column(
+                                                                crossAxisAlignment:
+                                                                    CrossAxisAlignment
+                                                                        .start,
+                                                                children: [
+                                                                  Icon(
+                                                                    Icons
+                                                                        .pedal_bike_sharp,
+                                                                    size: 40,
+                                                                  ),
+                                                                ],
+                                                              )),
+                                                          Expanded(
+                                                            flex: 2,
+                                                            child: Column(
+                                                              mainAxisAlignment:
+                                                                  MainAxisAlignment
+                                                                      .spaceBetween,
+                                                              children: [
+                                                                Text(
+                                                                  bikes[index]
+                                                                      .label!,
+                                                                  style:
+                                                                      GoogleFonts
+                                                                          .acme(
+                                                                    color: Colors
+                                                                        .blue,
+                                                                    fontSize: 16,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .bold,
+                                                                    letterSpacing:
+                                                                        2,
+                                                                  ),
+                                                                ),
+                                                                Text(
+                                                                  bikes[index]
+                                                                      .model!,
+                                                                  style:
+                                                                      GoogleFonts
+                                                                          .acme(
+                                                                    color: Colors
+                                                                        .blue,
+                                                                    fontSize: 14,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w500,
+                                                                    letterSpacing:
+                                                                        1,
+                                                                  ),
+                                                                ),
+                                                                Text(
+                                                                  "KM:" +
+                                                                      bikes[index]
+                                                                          .traveledKm!
+                                                                          .toString(),
+                                                                  style:
+                                                                      GoogleFonts
+                                                                          .acme(
+                                                                    color: Colors
+                                                                        .blue,
+                                                                    fontSize: 14,
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w500,
+                                                                    letterSpacing:
+                                                                        1,
+                                                                  ),
+                                                                ),
+                                                              ],
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                );
+                                              },
+                                            ),
+                                          );
+                                        }
+                                        // Caso nenhum dado esteja disponível
+                                        return Center(
+                                            child: Text(
+                                          "Nenhum dado disponível.",
+                                          style: GoogleFonts.acme(
+                                            color: Colors.blue,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500,
+                                            letterSpacing: -1.2,
+                                          ),
+                                        ));
+                                      },
+                                    ),
+                                 
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              )
             ],
           )),
       bottomNavigationBar: const BottomBar(selectedIndex: 2),
